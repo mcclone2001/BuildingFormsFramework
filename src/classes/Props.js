16a30eccbccafb515497db74f8710186c29029bd
Iván Quiñones <mcclone2001@gmail.com>
2018-11-29 22:47:36 -0600

    Extrayendo Campos de prueba a un archivo individual
    Quitamos el hardcode de campos de prueba para poder incorporar carga desde una fuente de datos en otro momento

--- src/classes/Props.js
+++ src/classes/Props.js
@@ -2,6 +2,7 @@ import Elemento from '@/components/Elemento'
 import TipoDeCampo from '@/classes/TipoDeCampo'
 
 const CrearCampoDesdeJSON = function (jsonCampo) {
+  // https://stackoverflow.com/questions/34655616/create-an-instance-of-a-class-in-es6-with-a-dynamic-name/34656123
   return new MapeoNombreAClase[jsonCampo.tipo](jsonCampo.parametros)
 }
 
--- src/components/Elemento.vue
+++ src/components/Elemento.vue
@@ -20,16 +20,18 @@ export default{
   methods: {
     generarUUID: function () {
       //  https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
-      console.log('generando un uuid')
+      console.log('generando un uuid para ' + this.etiqueta)
       var d = new Date().getTime()
       if (typeof performance !== 'undefined' && typeof performance.now === 'function') {
         d += performance.now() //  use high-precision timer if available
       }
-      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
         var r = (d + Math.random() * 16) % 16 | 0
         d = Math.floor(d / 16)
         return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16)
       })
+      console.log(uuid)
+      return uuid
     },
 
     codificarEntidadesHTML: function (cadena) {
