1b7e0c953a5162644a98c216e516749b38c93583
Iván Quiñones <mcclone2001@gmail.com>
2018-12-05 22:49:39 -0600

    WYSIWYG - Listar Campos
    Primer borrador Listar campos en el toolbox

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,7 +1,16 @@
 <template>
   <b-container class="bv-example-row">
       <b-row>
-          <b-col>Toolbox</b-col>
+          <b-col>
+            <h2>Toolbox</h2>
+            <b-list-group v-for="campo in d_campos" :key="campo">
+              <b-list-group-item>
+                {{ campo }}
+                { Preview }
+                <b-button>+</b-button>
+              </b-list-group-item>
+            </b-list-group>
+          </b-col>
           <b-col>Preview</b-col>
           <b-col>Selection properties</b-col>
       </b-row>
@@ -9,7 +18,14 @@
 </template>
 
 <script>
+  import MapeoNombreClaseDeDatosANombreComponente from '@/classes/catalogs/MapeoNombreClaseDeDatosANombreComponente'
+
   export default {
-    name: 'WYSIWYG'
+    name: 'WYSIWYG',
+    data: function () {
+      return {
+        d_campos: MapeoNombreClaseDeDatosANombreComponente
+      }
+    }
   }
 </script>
